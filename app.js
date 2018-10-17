11a41925531f2de49d1719768e78367c5e7abf9e
ida16 <34029551+ida16@users.noreply.github.com>
2018-10-17 17:21:56 +0000

    eytt greetings

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // app.js
 
-const greetings = require('./src/greetings.js');
+const express = require("express");
+const app = express();
+const api = require("./src/api");
 
-console.log(greetings('Totoro'));
+app.use("/api", api);
+
+app.get("*", (req, res) => {
+    res.send({error: "No routine defined"});
+});
+
+module.exports = app;
